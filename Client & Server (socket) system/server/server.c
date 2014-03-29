7b9cc4f31ab64df21c471c207285dac2e893f713
syvjohan <johanfredriksson1987@gmail.com>
2014-03-29 19:33:25 +0100

    basic server finnish

---
+++ Client & Server (socket) system/server/server.c
@@ -0,0 +1,57 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+	int listening = 0;
+	int connect = 0;
+	struct sockaddr_in serv_addr;
+	const int port = 12345;
+
+	time_t ticks;
+
+	char buff[1025];
+
+	//<sys/socket.h>
+	// AF_INET This designates the address format that goes with the Internet namespace.
+	//The SOCK_STREAM style is like a pipe. it operates over a connection with a particular remote socket, and transmits data reliably as a stream of bytes.
+	listening = socket(AF_INET, SOCK_STREAM, 0);
+	//<string.h>
+	// copies the character c (an unsigned char) to the first n characters of the string pointed to by the argument str.
+	memset(&serv_addr, '0', sizeof(serv_addr));
+	memset(buff, '0', sizeof(buff));
+
+	serv_addr.sin_family = AF_INET;
+	// Alllows the program to run without knowing the ip-address.
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// the port.
+	serv_addr.sin_port = htons(port);
+
+	// binds the ip-address to the socket
+	bind(listening, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	// specifies maximum number of client connections that server will queue for this listening socket.
+	listen(listening, 10);
+
+	while(1) {
+		connect = accept(listening, (struct sockaddr*) NULL, NULL);
+
+		ticks = time(NULL);
+		//snprintf = redirects the output of printf to a buffer
+		// ctime = returns a string representing the localtime based on the argument timer.
+		snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+		write(connect, buff, strlen(buff));
+
+		close(connect);
+		// sleep for one second. 
+		// prevents resources from beeing wasted. 
+		sleep(1);
+
+	}
+}
