05d1e704d0d739f12ef69f60a05925ec89b1641f
syvjohan <johanfredriksson1987@gmail.com>
2014-10-16 17:05:01 +0200

    write comments

--- Inlämning/Inlämning3/Inlämning3_AB5785/Inlämning3_AB5785/main.c
+++ Inlämning/Inlämning3/Inlämning3_AB5785/Inlämning3_AB5785/main.c
@@ -4,18 +4,22 @@
 
 #define SIZE 10
 
+//Fills array with random values. The number of values in array is determined
+//by the macro SIZE.
 void fill_rand_ver1(int array[], int size) {
 	int i;
-	for (i = 0; i != size; ++i) {
+	for (i = 0; i != size; i++) {
+		//srand(), size sets the range and +1 sets the range from 1 - 10 instead of 0 -10.
 		array[i] = ((rand() % size) + 1);
 	}
 }
 
+//Function contains 2 foor loops, one inner and one outer loop.
 void sort_ver1(int array[], int size) {
 	int i, j, temp;
-	for (i = 0; i < size; i++) {
+	for (i = 0; i != size; i++) {
 		for (j = i + 1; j != size; j++) {
-			if (array[j] < array[i]) {
+ 			if (array[j] < array[i]) {
 				//Swap.
 				temp = array[j];
 				array[j] = array[i];
@@ -27,6 +31,7 @@ void sort_ver1(int array[], int size) {
 
 void print_values_ver1(char string[], int array[], int size) {
 	int i;
+	//Loops until the end sign ('\0') in the array.
 	for (i = 0; string[i] != '\0'; i++) {
 		printf("%c", string[i]);
 	}
@@ -37,11 +42,15 @@ void print_values_ver1(char string[], int array[], int size) {
 	printf("\n\n");
 }
 
-
+//In c a pointer is the same as an array. The pointer name points to the first
+//number in the array. 
 void fill_rand_ver2(int *pointer, int size) {
 	int *pi;
+	//pointer + size the pointer always points to one integer even if it is incremented it jump
+	//one step ahead in the pointer (container) thats why + size need to be there otherwise there would be only one iteration.
 	for (pi = pointer; pi != (pointer + size); pi++) {
 		int r = ((rand() % size) + 1);
+		//*pi points to the value. pi points to the memory location.
 		*pi = r;
 	}
 }
@@ -61,13 +70,16 @@ void sort_ver2(int *pointer, int size) {
 		}
 	}
 }
+
 void print_values_ver2(char *string, int *pointer, int size) {
 	int *pi;
 	int i;
 	char *pc;
 	pc = string;
 	pi = pointer;
-
+	//A string is a char array. 
+	//The implementation of printf() sees the "%s" and assumes that the corresponding argument
+	//is a pointer to an char and uses the pointer to traverse the string and print it.
 	printf("%s", pc);
 	printf("\n");
 	for (pi; pi != (pointer + size); pi++) {
@@ -76,6 +88,7 @@ void print_values_ver2(char *string, int *pointer, int size) {
 	printf("\n\n");
 }
 
+//Fills the occupied places in the array with '\0'.
 void emptyArray(int array[], int size) {
 	int i;
 	for (i = 0; i != size; i++) {
