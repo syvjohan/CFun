59fe0aedfe8f6b4dc1bf27299ed7ead27f5274b4
syvjohan <johanfredriksson1987@gmail.com>
2014-11-28 22:52:32 +0100

    QuickSort has syntax errors

--- Inlämning/Inlämning6/Inlämning6/Array.h
+++ Inlämning/Inlämning6/Inlämning6/Array.h
@@ -1,11 +1,13 @@
 #pragma once
-#include "Integer.h"
+#include "Number.h"
 #include "Defs.h"
 
 #include <time.h>
 #include <iostream>
 #include <cassert>
+#include <algorithm>
 
+//Forward declaration off the class
 template<typename T>
 class Array;
 
@@ -16,26 +18,29 @@ template<typename T>
 bool operator!=( Array<T> &arr1, Array<T> &arr2);
 
 template<typename T>
+bool operator==( Number<T> &arr1, T &number);
+	
+template<typename T>
+bool operator!=( Number<T> &arr1, T &number);
+
+template<typename T>
 std::ostream& operator<<( std::ostream& cout, Array<T> arr);
 
 template<typename T>
 class Array {
 public:
-	friend Integer;
 	Array<T>(const int size);
 	Array<T>();
 	Array<T>(const T* data);
 	Array<T>(const Array<T> &rhs);
-	//No constructor for adding one single number se PushBack function instead...
-
-
+	//No constructor for adding one single number se PushBack function and operators instead...
+	
 	~Array<T>();
 
-	void QuickSort(T &arr, T left, T right);
+	void Sort(T begin, T end);
 	void FillArr(T min, T max);
-	T& At(T i);
-
-	T& operator[]( const int index);
+	
+	Number<T>& operator[]( T index);
 
 	Array<T>& operator=(const Array<T>& rhs);
 	Array<T>& operator=(const T* numbers);
@@ -47,50 +52,57 @@ public:
 	Array<T> operator+(const Array<T>& rhs);
 	Array<T> operator+(const T* numbers);
 
-	T& At(T i) const;
+	Number<T>& At(const int i);
+	Number<T>& At(const int i) const;
 	void ShrinkToFit();
 	int Length() const;
-	const T* Data() const;
+	const Number<T> *Data() const;
 	int Capacity() const;
 	void PushBack(const T number);
-	void Reserve(const int sizeToReserve);
+	void Reserve(const int newSize);
+	void Empty();
 
+	friend bool operator==(const Number<T> &arr1, const T &number);
 	friend bool operator==(const Array<T> &arr1, const Array<T> &arr2);
+
 	friend bool operator!=(const Array<T> &arr1, const Array<T> &arr2);
+	friend bool operator!=(const Number<T> &arr1, const T &number);
 
 	friend std::ostream& operator<<(std::ostream& cout, const Array<T> arr);
 
 private:
 	int size;
 	int length;
-	Integer *integerArr;
+	Number<T> *numberArr;
 
-	T Partition(T &arr, T left, T right);
-	bool CmpArr(const Array<T> &arr1, const Array<T> &arr2);
+	void QuickSort(Number<T> *arr, T left, T right);
+	T Partition(Number<T> *arr, T left, T right);
+	bool CmpArrVSArr(const Array<T> &arr1, const Array<T> &arr2);
+	bool CmpArrVST(const Array<T> &arr1, const T &number);
 	T GenerateRandNumbers(T min, T max);
 	void SetSeed();
-	void ArrLength(const T *numbers) const;
+	int ArrLength(const T *numbers) const;
 	void Concat(const Array<T> &r);
 };
 
 //Default constructor.
 template<typename T>
 Array<T>::Array() {
-	integerArr = NULL;
+	numberArr = NULL;
 	size = 0;
 	length = 0;
 	SetSeed();
 }
 
-//Int rounds off input value to an integer. Works as std::basic_string Reserve function.
+//Int rounds off input value to an Number. Works as std::basic_string Reserve function.
 template<typename T>
 Array<T>::Array(const int size) {
-	integerArr = NULL;
+	numberArr = NULL;
 	this->size = size;
 	length = 0;
 	SetSeed();
 
-	integerArr = DBG_NEW Integer[size];
+	numberArr = DBG_NEW Number<T>[size];
 	
 }
 
@@ -98,17 +110,17 @@ template<typename T>
 Array<T>::Array(const T* data) {
 	length = ArrLength(data);
 	size = length;
-	integerArr = DBG_NEW Integer[size];
+	numberArr = DBG_NEW Number<T>[size];
 
 	SetSeed();
 
-	memcpy(integerArr, data, length);
+	memcpy(numberArr, data, length);
 }
  
 //Copy constructor.
 template<typename T>
 Array<T>::Array(const Array<T> &rhs) {
-	integerArr = NULL;
+	numberArr = NULL;
 	size = 0;
 	length = 0;
 	SetSeed();
@@ -117,8 +129,8 @@ Array<T>::Array(const Array<T> &rhs) {
 
 template<typename T>
 Array<T>::~Array() {
-	if (integerArr != NULL) {
-		delete[] integerArr;
+	if (numberArr != NULL) {
+		delete[] numberArr;
 	}
 }
 
@@ -129,18 +141,18 @@ void Array<T>::SetSeed() {
 
 //Index operator
 template<typename T>
-inline T& Array<T>::operator[](const int index) {
-	return integerArr[index];
+inline Number<T>& Array<T>::operator[](T index) {
+	return numberArr[index];
 }
 
 template<typename T>
 Array<T>& Array<T>::operator=(const Array<T>& rhs) {
-	if (rhs.integerArr == integerArr) {
+	if (rhs.numberArr == numberArr) {
 		return *this;
 	}
-
-	delete[] integerArr;
-	integerArr = NULL;
+	
+	delete[] numberArr;
+	numberArr = NULL;
 	length = 0;
 	size = 0;
 	Concat(rhs);
@@ -159,7 +171,7 @@ Array<T>& Array<T>::operator=(T number) {
 		Concat(number);
 	}
 
-	integerArr[0] = number;
+	numberArr[0] = number;
 
 	return *this;
 }
@@ -190,20 +202,21 @@ Array<T> Array<T>::operator+(const T* numbers) {
 	return a;
 }
 
+
 template<typename T>
-inline T& Array<T>::At(T i) const {
+inline Number<T>& Array<T>::At(const int i) const {
 	if (i >= size || i < 0) {
 		throw std::out_of_range("Index of array is out of bounds!");
 	}
-	return integerArr[i];
+	return numberArr[i];
 }
 
 template<typename T>
-inline T& Array<T>::At(T i) {
+inline Number<T>& Array<T>::At(const int i) {
 	if (i >= size || i < 0) {
 		throw std::out_of_range("Index of array is out of bounds!");
 	}
-	return integerArr[i];
+	return numberArr[i];
 }
 
 template<typename T>
@@ -212,8 +225,8 @@ int Array<T>::Length() const {
 }
 
 template<typename T>
-const T* Array<T>::Data() const {
-	return integerArr;
+const Number<T>* Array<T>::Data() const {
+	return numberArr;
 }
 
 template<typename T>
@@ -225,11 +238,59 @@ template<typename T>
 void Array<T>::ShrinkToFit() {
 	if (size > length) {
 		size = length;
-		T *temp = integerArr;
-		integerArr = DBG_NEW Integer[length];
-		memcpy(integerArr, temp, length);
+		Number<T> *temp = numberArr;
+		numberArr = DBG_NEW Number<T>[length];
+		memcpy(numberArr, temp, length);
 		delete[] temp;
 	}
+}
+
+template<typename T>
+void Array<T>::Reserve(const int newSize) {
+	if (newSize <= size) {
+		return;
+	}
+
+	size = newSize;
+	T *temp = DBG_NEW T[size];
+	memcpy(numberArr, temp, size);
+	delete[] temp;
+}
+
+template<typename T>
+void Array<T>::Concat(const Array<T> &r) {
+	int newLength = length + r.length;
+
+	if (numberArr == NULL) {
+		numberArr = DBG_NEW Number<T>[size];
+
+	} else if (newLength > size) {
+		int newSize = std::max(newLength, size * 2);
+
+		Number<T> *temp = numberArr;
+		numberArr = DBG_NEW Number<T>[size];
+		size = newSize;
+		memcpy(numberArr, temp, length);
+		delete[] temp;
+	}
+
+	memcpy(numberArr + length, r.numberArr, r.length);
+	length = newLength;
+}
+
+template<typename T>
+void Array<T>::PushBack(const T number) {
+	Concat(&number);
+}
+
+//It is more efficient to set the array length, capacity to zero and the first element to NULL instead of emptying the array.
+//Nothing will be achived with emptying the values in the array, thats not how array works...
+//The values that is left will be overwriten on next fill. This is the same technique as std::basic_string uses.
+template<typename T>
+void Array<T>::Empty() {
+	numberArr[0] = NULL;
+	size = 0;
+	length = 0;
 }
 
 template<typename T>
@@ -242,70 +303,38 @@ T Array<T>::GenerateRandNumbers(T min, T max) {
 	return value;
 }
 
-template<typename T>
-void Array<T>::Reserve(const int newSize) {
-	if (newSize <= size) {
-		return;
-	}
-
-	size = newSize;
-	T *temp = DBG_NEW T[size];
-	memcpy(integerArr, temp, size);
-	delete[] temp;
-}
-
-template<typename T>
-void Array<T>::Concat(const Array<T> &r) {
-	int newLength = length + r.length;
-
-	if (integerArr == NULL) {
-		integerArr = DBG_NEW Integer[size];
-
-	} else if (newLength > size) {
-		int newSize = std::max(newLength, size * 2);
-
-		T *temp = integerArr;
-		integerArr = DBG_NEW[size];
-		size = newSize;
-		memcpy(integerArr, temp, length);
-		delete[] temp;
-	}
-
-	memcpy(integerArr + length, r.integerArr, r.length);
-	length = newLength;
-}
-
-template<typename T>
-void Array<T>::PushBack(const T number) {
-	Concat(&number);
-}
 
 template<typename T>
 void Array<T>::FillArr(T min, T max) {
 	 do {
 			T value = GenerateRandNumbers(min, max);
-			integerArr[length] = value;
+			numberArr[length] = value;
 			length++;
 	 } while (size != length);
 }
 
+//QuickSort interface.
 template<typename T>
-void Array<T>::QuickSort(T &arr, T left, T right) {
-	T currValue;
-	if (right < left) {
-		currValue = Partion(arr, right, left);
-		QuickSort(arr, right, currValue); //sort from mid to start.
-		QuickSort(arr, currValue + 1, left); //sort from mid to end.
-	}
-	return;
+void Array<T>::Sort(T begin, T end) {
+	QuickSort(numberArr, begin, end);
 }
 
 template<typename T>
-T Array<T>::Partition(T &arr, T left, T right) {
-	T x = arr[right];
-	T i = right - 1;
-	T j = left + 1;
-	T temp;
+void Array<T>::QuickSort(Number<T> *arr, T left, T right) {
+	T currValue; 
+	if (right < left) {
+		currValue = Partition(arr, right, left);
+		QuickSort(arr, right, currValue); //sort from mid to start.
+		QuickSort(arr, currValue + 1, left); //sort from mid to end.
+	}
+}
+
+template<typename T>
+T Array<T>::Partition(Number<T> *arr, T left, T right) {  
+	T x = arr[right]; 
+	T i = right; //- 1
+	T j = left; // + 1
+	T temp; 
 	do {
 		do {
 			j--;
@@ -325,7 +354,7 @@ T Array<T>::Partition(T &arr, T left, T right) {
 
 //Counts the elements and return the length. change NULL to '/0' and you have  c version of strlen
 template<typename T>
-void Array<T>::ArrLength(const T *numbers) const {
+int Array<T>::ArrLength(const T *numbers) const {
 	size_t len = 0;
 	while (*numbers != NULL) {
 		*numbers++;
@@ -336,21 +365,39 @@ void Array<T>::ArrLength(const T *numbers) const {
 }
 
 template<typename T>
-bool Array<T>::CmpArr(const Array<T> &arr1, const Array<T> &arr2) {
+bool Array<T>::CmpArrVSArr(const Array<T> &arr1, const Array<T> &arr2) {
 	memcmp(arr1, arr2, sizeof(arr1)) ? true : false;
 }
 
 template<typename T>
+bool Array<T>::CmpArrVST(const Array<T> &arr1, const T &number) {
+	return arr1[0] == number;
+}
+
+template<typename T>
 inline bool operator==(const Array<T> &arr1, const Array<T> &arr2) {
-	return CmpArr(arr1, arr2);
+	return CmpArrVSArr(arr1, arr2);
 }
 
 template<typename T>
 inline bool operator!=(const Array<T> &arr1, const Array<T> &arr2) {
-	return operator!=(arr1, arr2);
+	return !CmpArrVSArr(arr1, arr2);
+}
+
+template<typename T>
+inline bool operator==(const Number<T> &arr1, const T &number) {
+	return CmpArrVSArr(arr1, number);
+}
+
+template<typename T>
+inline bool operator!=(const Number<T> &arr1, const T &number) {
+	return !CmpArrVSArr(arr1, number);
 }
 
 template<typename T>
 inline std::ostream& operator<<(std::ostream& cout, const Array<T> arr) {
-	return cout << arr;
+	for (int i = 0; i != length; ++i) {
+		cout << arr[i] << " ";
+	}
+	return;
 }
--- Inlämning/Inlämning6/Inlämning6/Main.cpp
+++ Inlämning/Inlämning6/Inlämning6/Main.cpp
@@ -8,21 +8,82 @@ typedef Array<double> arrDouble;
 typedef Array<short> arrShort;
 
 void Test() {
-
-	Array<int> arrI1, arrI2(3), arrI3(3);
+	
+	Array<int> arrI1, arrI2(3), arrI3(3), arrI4(7);
 	//Array<double> arrD1, arrD2(10.00), arrD3(10.00);
 	//Array<short> arrS1, arrS2(10), arrS(10);
 
-	arrI3.FillArr(2, 6);
-	/*assert(arr1[10] == 7);
-	assert(arr1[0] == 1);*/
 
-	arrI2.FillArr(2, 6);
-	//assert(arrI1 == arrI2);
+	// FillArray
+	arrI3.FillArr(2, 26);
 
+	//Checks if the seed is reprodicing a new value.
+	arrI2.FillArr(2, 26);
+	//Operator ==
+	assert(arrI1 == arrI2);
 
-	//std::cout << arrI2;
+	arrI4.FillArr(2, 26);
 
+	//Operator !=
+	assert(arrI3 != arrI4);
+
+	//Operator =
+	arrI3 = arrI4;
+
+	// Data()
+	assert(arrI3.Data() != arrI4.Data());
+
+	//Reserve
+	arrI3.Reserve(15);
+
+	//Capacity
+	assert(arrI3.Capacity() == arrI4.Capacity());
+
+	//ShrinkToFit
+	arrI3.ShrinkToFit();
+	assert(arrI3 != arrI4);
+
+	// PushBack
+	arrI4.PushBack(5);
+
+	// Length
+	assert(arrI4.Length() == arrI3.Length());
+
+	//Empty
+	arrI3.Empty();
+	assert(arrI3.Data() != NULL);
+	
+	// At.
+	arrI3.PushBack(5);
+	assert(arrI3.At(0) != 5);
+
+	//Empty
+	arrI2.Empty();
+	assert(arrI3.Data() != NULL);
+	//Empty
+	arrI3.Empty();
+	assert(arrI3.Data() != NULL);
+	//Empty
+	arrI4.Empty();
+	assert(arrI3.Data() != NULL);
+
+	//QuickSort
+	arrI2.FillArr(0, 10);
+	std::cout << arrI2.Data();
+
+	arrI2.Sort(0, 10);
+	std::cout << arrI2.Data();
+
+	//Operator =
+	arrI3 = 2;
+	//Operator []
+	assert(arrI3[0] != 2);
+
+	// Operator +
+	arrI3 + 3;
+	arrI4 + 45;
+	arrI3 + arrI4;
+	assert(arrI3 != 2345);
 
 }
 
